1f4b0314c8a860d683bbb5be0d23ae905d46d351
David Evans <davidje13@users.noreply.github.com>
2018-09-29 18:50:03 +0100

    Favicons

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,8 @@
 import Dice from './dice/dice.js';
 
 const container = document.getElementById('content');
+const faviconLink = document.getElementById('favicon');
 
 const dice = new Dice(container);
 dice.begin();
+faviconLink.setAttribute('href', 'resources/dice/favicon.png');
