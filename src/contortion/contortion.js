c718fecf78f7f8e7e7e5bc4fbd771488abe03c1e
David Evans <davidje13@users.noreply.github.com>
2018-09-30 02:24:05 +0100

    Add needle to contortion spinner

--- src/contortion/contortion.js
+++ src/contortion/contortion.js
@@ -1,7 +1,7 @@
-const BOARD_WIDTH = 280;
-const BOARD_HEIGHT = 280;
-const SPINNER_SIZE = 220;
-const SPINNER_CORE_SIZE = 136;
+const BOARD_WIDTH = 310;
+const BOARD_HEIGHT = 310;
+const SPINNER_SIZE = 250;
+const SPINNER_CORE_SIZE = 160;
 
 function make(tag, className) {
 	const o = document.createElement('div');
@@ -33,6 +33,10 @@ export default class Contortion {
 		segmentsCore.style.marginLeft = `${-SPINNER_CORE_SIZE / 2}px`;
 		segmentsCore.style.marginTop = `${-SPINNER_CORE_SIZE / 2}px`;
 
+		const needle = make('div', 'needle');
+		const needleShadow = make('div', 'needle-shadow');
+		const pin = make('div', 'pin');
+
 		segments.appendChild(make('div', 'hbeam'));
 		segments.appendChild(make('div', 'vbeam'));
 		segments.appendChild(segmentsCore);
@@ -43,6 +47,9 @@ export default class Contortion {
 		this.board.appendChild(make('div', 'right-hand'));
 		this.board.appendChild(make('div', 'left-foot'));
 		this.board.appendChild(make('div', 'right-foot'));
+		this.board.appendChild(needleShadow);
+		this.board.appendChild(needle);
+		this.board.appendChild(pin);
 		this.inner.appendChild(this.board);
 	}
 
