e683aa7c60fb6204ed1517d444d6ef9c0a4f5733
David Evans <davidje13@users.noreply.github.com>
2018-09-30 15:11:12 +0100

    Background for answers ball

--- src/answers/answers.js
+++ src/answers/answers.js
@@ -1,6 +1,38 @@
+const BALL_SIZE = 350;
+const HOLE_SIZE = 180;
+const CHAMFER_SIZE = 3;
+
+function make(tag, className) {
+	const o = document.createElement(tag);
+	o.className = className;
+	return o;
+}
+
+function setSize(o, size) {
+	o.style.width = `${size}px`;
+	o.style.height = `${size}px`;
+	o.style.marginLeft = `${-size / 2}px`;
+	o.style.marginTop = `${-size / 2}px`;
+}
+
 export default class Answers {
 	constructor() {
 		this.inner = document.createElement('div');
+
+		const ball = make('div', 'ball');
+		setSize(ball, BALL_SIZE);
+
+		const chamfer = make('div', 'chamfer');
+		setSize(chamfer, HOLE_SIZE + CHAMFER_SIZE * 2);
+
+		const hole = make('div', 'hole');
+		setSize(hole, HOLE_SIZE);
+
+		ball.appendChild(make('div', 'shine'));
+		ball.appendChild(chamfer);
+		ball.appendChild(hole);
+
+		this.inner.appendChild(ball);
 	}
 
 	title() {
