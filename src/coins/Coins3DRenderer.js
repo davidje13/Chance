56ce732a90ecacfe4d57661eef8a5bb644ff9cd6
David Evans <davidje13@users.noreply.github.com>
2018-10-18 20:57:48 +0100

    Add simple coin rendering

--- src/coins/Coins3DRenderer.js
+++ src/coins/Coins3DRenderer.js
@@ -2,7 +2,7 @@ import Canvas from '../3d/Canvas.js';
 import Program from '../3d/Program.js';
 import {VertexShader, FragmentShader} from '../3d/Shader.js';
 import {Texture2D} from '../3d/Texture.js';
-import Face from '../3d/Face.js';
+import Box from '../3d/Box.js';
 import DepthFrag from '../3d/DepthFrag.js';
 import {M4} from '../math/Matrix.js';
 import Quaternion from '../math/Quaternion.js';
@@ -16,23 +16,26 @@ const PROG_SHAPE_VERT = `
 	uniform lowp mat4 projview;
 
 	attribute vec4 pos;
-	attribute vec3 norm;
-	attribute vec2 tex;
 
 	varying lowp vec3 p;
 	varying lowp vec3 n;
 	varying lowp vec2 t;
+	varying lowp float thickness;
 
 	void main() {
 		p = pos.xyz;
-		n = norm;
-		t = tex;
+		n = vec3(0.0, 0.0, sign(p.z));
+		if (p.z > 0.0) {
+			t = vec2(-p.x, p.y) * 0.25 + vec2(0.25, 0.25);
+		} else {
+			t = p.xy * 0.25 + vec2(0.75, 0.25);
+		}
+		thickness = abs(pos.z);
 		gl_Position = projview * pos;
 	}
 `;
 
 const PROG_SHAPE_FRAG = DepthFrag({layerSteps: 12, binarySearchSteps: 3, linearInterpolation: true}) + `
-	uniform sampler2D atlas;
 	uniform sampler2D normalMap;
 	uniform lowp mat3 rot;
 	uniform highp vec3 eye;
@@ -41,27 +44,60 @@ const PROG_SHAPE_FRAG = DepthFrag({layerSteps: 12, binarySearchSteps: 3, linearI
 	varying lowp vec3 p;
 	varying lowp vec3 n;
 	varying lowp vec2 t;
+	varying lowp float thickness;
 
 	const lowp vec3 ambientCol = vec3(0.3);
 	const lowp vec3 lightCol = vec3(0.7);
+	const lowp vec4 shineCol = vec4(1.0, 1.0, 0.6, 0.5);
 	const lowp vec3 lightDir = vec3(0.0, 0.0, 1.0);
+	const lowp vec3 shineDir = normalize(vec3(0.0, 1.0, 2.0));
+	const lowp vec3 baseCol = vec3(0.82, 0.78, 0.33);
 
 	lowp vec3 normalAt(in lowp vec2 uv) {
 		return texture2D(normalMap, uv).xyz * 2.0 - 1.0;
 	}
 
-	lowp vec4 colourAt(in lowp vec2 uv) {
-		return texture2D(atlas, uv);
+	lowp vec4 metal(in lowp vec3 norm, in lowp vec3 ref) {
+		return vec4(
+			mix(
+				baseCol * (ambientCol + lightCol * dot(rot * norm, lightDir)),
+				shineCol.rgb,
+				smoothstep(0.49, 0.51, dot(rot * ref, shineDir)) * shineCol.a
+			),
+			1.0
+		);
 	}
 
 	void main() {
 		lowp vec3 ray = normalize(p - eye);
+		if (length(p.xy) > 1.0) {
+			highp float ee = dot(eye.xy, eye.xy);
+			highp float er = dot(eye.xy, ray.xy);
+			highp float rr = dot(ray.xy, ray.xy);
+			highp float root = er * er - ee * rr + rr;
+			if (root < 0.0) {
+				discard;
+			}
+			root = sqrt(root);
+			highp float l = (-er - root) / rr;
+			lowp vec3 pos = eye + ray * l;
+			lowp vec3 norm = vec3(pos.xy, 0.0);
+			if (pos.z < -thickness || pos.z > thickness) {
+				discard;
+			}
+			lowp float bump = atan(pos.y / pos.x) * 200.0;
+			norm.x += sin(bump) * 0.5;
+			norm.y += cos(bump) * 0.5;
+			norm = normalize(norm);
+			gl_FragColor = metal(norm, reflect(ray, norm));
+			return;
+		}
 
 		lowp vec2 uv = t;
 		lowp mat3 faceD;
 		faceD[2] = n;
 		faceD[1] = vec3(0.0, 1.0, 0.0);
-		faceD[0] = cross(faceD[1], faceD[2]);
+		faceD[0] = cross(faceD[2], faceD[1]);
 		lowp vec3 texSpaceRay = ray * faceD;
 		lowp float zmult = -1.0 / texSpaceRay.z;
 		lowp vec2 duv = texSpaceRay.xy * zmult * 0.5;
@@ -71,16 +107,11 @@ const PROG_SHAPE_FRAG = DepthFrag({layerSteps: 12, binarySearchSteps: 3, linearI
 		lowp float depthLimit = min(maxDepth, min(dd.x, dd.y));
 		lowp float depth = depthAt(normalMap, uv, duv, maxDepth, depthLimit);
 		lowp vec3 pos = p + ray * depth * zmult;
-		uv.xy += duv * depth;
+		uv += duv * depth;
 
 		lowp vec3 norm = faceD * normalAt(uv);
 
-		lowp vec4 faceTex = colourAt(uv);
-
-		gl_FragColor = vec4(
-			colourAt(uv).rgb * (ambientCol + lightCol * dot(rot * norm, lightDir)),
-			1.0
-		);
+		gl_FragColor = metal(norm, reflect(ray, norm));
 	}
 `;
 
@@ -103,28 +134,19 @@ export default class Coins3DRenderer {
 		gl.cullFace(gl.BACK);
 		gl.enable(gl.CULL_FACE);
 
-		this.shape = new Face({size: {width: 2.0, height: 2.0}, twoSided: true});
+		this.shape = new Box({width: 2.1, height: 2.1, depth: 0.15});
 
 		this.prog = new Program(gl, [
 			new VertexShader(gl, PROG_SHAPE_VERT),
 			new FragmentShader(gl, PROG_SHAPE_FRAG),
 		]);
 
-		const maxDepth = 0.5;
+		const maxDepth = 0.015;
 		const worldFaceWidth = 2.0;
-		const normalMapFaceWidth = 1.0;
+		const normalMapFaceWidth = 0.5;
 		const normalMapFaceDepth = maxDepth * normalMapFaceWidth / worldFaceWidth;
 
 		this.maxDepth = maxDepth;
-
-		this.atlas = new Texture2D(gl, {
-			[gl.TEXTURE_MAG_FILTER]: gl.LINEAR,
-			[gl.TEXTURE_MIN_FILTER]: gl.LINEAR,
-			[gl.TEXTURE_WRAP_S]: gl.CLAMP_TO_EDGE,
-			[gl.TEXTURE_WRAP_T]: gl.CLAMP_TO_EDGE,
-		});
-		this.atlas.setSolid(0, 0, 0, 0);
-		this.atlas.loadImage('resources/test/atlas.png');
 
 		this.normalMap = new Texture2D(gl, {
 			[gl.TEXTURE_MAG_FILTER]: gl.LINEAR,
@@ -133,7 +155,7 @@ export default class Coins3DRenderer {
 			[gl.TEXTURE_WRAP_T]: gl.CLAMP_TO_EDGE,
 		});
 		this.normalMap.setSolid(0.5, 0.5, 1.0, 1.0);
-		this.normalMap.generateNormalMap('resources/test/depth.png', normalMapFaceDepth);
+		this.normalMap.generateNormalMap('resources/coins/depth-gbp.png', normalMapFaceDepth);
 	}
 
 	resize(width, height) {
@@ -159,9 +181,6 @@ export default class Coins3DRenderer {
 				'eye': mView.invert().apply3([0, 0, 0]),
 				'rot': mView.as3(),
 				'pos': shape.boundVertices(),
-				'norm': shape.boundNormals(),
-				'tex': shape.boundUvs(),
-				'atlas': this.atlas,
 				'normalMap': this.normalMap,
 				'maxDepth': this.maxDepth,
 			});
