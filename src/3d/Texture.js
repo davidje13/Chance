399aeeced9614c891d7c2f49ed75345198e9eea7
David Evans <davidje13@users.noreply.github.com>
2018-11-06 22:34:10 +0000

    Add texture helper methods

--- src/3d/Texture.js
+++ src/3d/Texture.js
@@ -36,7 +36,7 @@ function lumToAlpha(data) {
 	return data;
 }
 
-function depthToNormals(data) {
+function depthToNormals(data, channelX, channelY) {
 	const w = data.width;
 	const h = data.height;
 	const d = data.data;
@@ -63,8 +63,83 @@ function depthToNormals(data) {
 			const dx = (dE - dW) * 2 + dNE - dNW + dSE - dSW;
 			const dy = (dS - dN) * 2 + dSE - dNE + dSW - dNW;
 			const p = (pC0 + x) * 4;
-			d[p    ] = dx * gradientScale / 8 + 128;
-			d[p + 1] = dy * gradientScale / 8 + 128;
+			d[p + channelX] = dx * gradientScale / 8 + 128;
+			d[p + channelY] = dy * gradientScale / 8 + 128;
+		}
+	}
+
+	return data;
+}
+
+function depthToOcclusion(data, channel) {
+	const w = data.width;
+	const h = data.height;
+	const d = data.data;
+
+	const occScale = 2.0;
+	const spread = 2;
+
+	for (let y = 0; y < h; ++ y) {
+		const pN0 = ((y + h - spread) % h) * w;
+		const pC0 = y * w;
+		const pS0 = ((y + spread) % h) * w;
+		for (let x = 0; x < w; ++ x) {
+			const xE = (x + spread) % w;
+			const xW = (x + w - spread) % w;
+
+			const d0  = d[(pC0 + x ) * 4 + 3];
+			const dN  = d[(pN0 + x ) * 4 + 3];
+			const dE  = d[(pC0 + xE) * 4 + 3];
+			const dS  = d[(pS0 + x ) * 4 + 3];
+			const dW  = d[(pC0 + xW) * 4 + 3];
+			const dNE = d[(pN0 + xE) * 4 + 3];
+			const dNW = d[(pN0 + xW) * 4 + 3];
+			const dSE = d[(pS0 + xE) * 4 + 3];
+			const dSW = d[(pS0 + xW) * 4 + 3];
+
+			const occ = d0 - ((dN + dE + dS + dW) * 2 + dNE + dNW + dSE + dSW) / 12;
+
+			d[(pC0 + x) * 4 + channel] = Math.floor(occ * occScale);
+		}
+	}
+
+	return data;
+}
+
+function blurChannel(data, channel) {
+	const w = data.width;
+	const h = data.height;
+	const d = data.data;
+
+	const input = new Uint8Array(w * h);
+
+	for (let i = 0; i < w * h; ++ i) {
+		input[i] = d[i * 4 + channel];
+	}
+	for (let y = 0; y < h; ++ y) {
+		const pN0 = ((y + h - 1) % h) * w;
+		const pC0 = y * w;
+		const pS0 = ((y + 1) % h) * w;
+		for (let x = 0; x < w; ++ x) {
+			const xE = (x + 1) % w;
+			const xW = (x + w - 1) % w;
+
+			const d0  = input[pC0 + x ];
+			const dN  = input[pN0 + x ];
+			const dE  = input[pC0 + xE];
+			const dS  = input[pS0 + x ];
+			const dW  = input[pC0 + xW];
+			const dNE = input[pN0 + xE];
+			const dNW = input[pN0 + xW];
+			const dSE = input[pS0 + xE];
+			const dSW = input[pS0 + xW];
+
+			const v = (
+				d0 * 0.5 +
+				(dN + dE + dS + dW) * 0.075 +
+				(dNE + dNW + dSE + dSW) * 0.05
+			);
+			d[(pC0 + x) * 4 + channel] = Math.round(v);
 		}
 	}
 
@@ -212,7 +287,7 @@ export default class Texture {
 		return loadImage(url)
 			.then(imageToData)
 			.then((data) => lumToAlpha(data))
-			.then((data) => depthToNormals(data))
+			.then((data) => depthToNormals(data, 0, 1))
 			.then((data) => downsample ? scaleDownsampledNormalMap(downsampleData(data)) : data)
 			.then((data) => this._uploadData(data, {premultiplyAlpha: false}));
 	}
@@ -220,7 +295,7 @@ export default class Texture {
 	convertNormalMap(data, {downsample = false} = {}) {
 		return Promise.resolve(data)
 			.then((data) => lumToAlpha(data))
-			.then((data) => depthToNormals(data))
+			.then((data) => depthToNormals(data, 0, 1))
 			.then((data) => downsample ? scaleDownsampledNormalMap(downsampleData(data)) : data)
 			.then((data) => this._uploadData(data, {premultiplyAlpha: false}));
 	}
